eef0497b0da763350acbdb8cbc0a0dc4581e1428
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-09-27 14:12:16 -0300

    chore: creating InviteModal

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { Copy, RefreshCw } from "lucide-react"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+import { useModal } from "@/hooks/use-modal-store"
+import { useOrigin } from "@/hooks/use-origin"
+
+export const InviteModal = () => {
+  const { isOpen, onClose, type, data } = useModal()
+  const origin = useOrigin()
+
+  const isModalOpen = isOpen && type === "invite"
+  const { server } = data
+
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Invite Friends
+          </DialogTitle>
+        </DialogHeader>
+        <div className="p-6">
+          <Label className="uppercase text-sm font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="flex items-center my-2 gap-x-2">
+            <Input
+              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+              value={inviteUrl}
+            />
+            <Button size="icon">
+              <Copy className="w-4 h-4" />
+            </Button>
+          </div>
+          <Button
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
+          >
+            Generate a new link
+            <RefreshCw className="w-4 h-4 ml-2" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
