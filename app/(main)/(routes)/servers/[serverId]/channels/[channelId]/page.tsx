1a12b7c122871abc8d08a7c438f754462754cc1c
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-10-02 08:59:20 -0300

    chore: creating channelId page

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,41 @@
-const ChannelIdPage = () => {
-  return ( 
-    <div>ChannelIdPage</div>
-   );
+import { redirect } from "next/navigation"
+import { redirectToSignIn } from "@clerk/nextjs"
+
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string
+    channelId: string
+  }
 }
- 
-export default ChannelIdPage;
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  })
+
+  if (!channel || !member) {
+    redirect("/")
+  }
+
+  return <div>ChannelIdPage</div>
+}
+
+export default ChannelIdPage
