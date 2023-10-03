5d3be7b16541db3e4828a364771333aeeb1c0a1c
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-10-03 13:26:33 -0300

    chore: create chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation"
 import { redirectToSignIn } from "@clerk/nextjs"
 
 import { ChatHeader } from "@/components/chat/chat-header"
+import { ChatInput } from "@/components/chat/chat-input"
 
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
@@ -43,6 +44,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   )
